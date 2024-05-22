ea0ba65305cd732ba416fcae4d9babd06b1e6e82
João Davi Mendes Correia <mendes.joaodavi@gmail.com>
2024-05-22 07:47:34 -0300

    adicionando mais exemplos

---
+++ desafio.js
@@ -0,0 +1,61 @@
+//1
+ //const texto = 'Olá, mundo!';
+ //const numero = 42;
+ //const booleano = true;
+
+ //console.log(typeof texto); // String
+ //console.log(typeof numero); // Number
+ //console.log(typeof booleano); // Boolean
+//  
+
+//2
+ //const primeiroNome = 'joão';
+ //const sobrenome = 'Mendes';
+ 
+ //console.log (`${primeiroNome} ${sobrenome}`);
+ //console.log (primeiroNome+ ' ' +sobrenome);
+//
+
+//3
+ //const numero = 30
+ //const string = ', é sua a idade' 
+ //console.log (numero+ '' +string)
+
+//4
+ //var qualquerCoisa = 'qualquer coisa';
+ //console.log(qualquerCoisa);
+
+ //qualquerCoisa = 'nova coisa';
+ //console.log(qualquerCoisa);
+
+ //5
+ //var fora = 'Fora do bloco';
+
+//if (true) {
+    //var dentro = 'Dentro do bloco';
+    //console.log(fora); // retorna 'Fora do bloco'
+    //console.log(dentro); // retorna 'Dentro do bloco'
+//}
+
+//console.log(fora); // retorna 'Fora do bloco'
+//console.log(dentro); // retorna 'Dentro do bloco'
+
+//let fora = 'Fora do bloco';
+
+//if (true) {
+    //let dentro = 'Dentro do bloco';
+    //console.log(fora); // retorna 'Fora do bloco'
+    //console.log(dentro); // retorna 'Dentro do bloco'
+//}
+
+//console.log(fora); // retorna 'Fora do bloco'
+//console.log(dentro); // erro 'dentro is not defined'
+
+//6
+//const estaChovendo = true or false
+
+//if (estaChovendo) {
+    //console.log('É melhor levar um guarda-chuva!');
+//} else {
+    //console.log('O tempo está bom, sem necessidade de guarda-chuva.');
+//}
