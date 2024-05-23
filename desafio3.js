99d29313f07eb89e5e932539cc74552e6f70ff2d
João Davi Mendes Correia <mendes.joaodavi@gmail.com>
2024-05-23 08:51:18 -0300

    adicionando mais

---
+++ desafio3.js
@@ -0,0 +1,39 @@
+//1
+ //let saldo = 3000
+ //const deposito = 1000
+ //const saque = 200
+ //const operacao = saldo + deposito - saque
+ 
+ //console.log ('antes do saque ' +saldo)
+ //console.log ('após a operação ' +operacao) 
+//2
+//const valor = 10
+ 
+ //if(valor % 2 === 0) {
+   // console.log('par')
+ //} else {
+    //console.log ('impar')
+ //}
+//3
+ //const logado = true
+ //const administrador = true
+ //if (logado && administrador){
+ // console.log('ACESSO LIBERADO')
+ //} else {
+    //console.log('ACESSO NEGADO')
+ // }
+//4
+ //condicao1 = false 
+ //condicao2 = true 
+ //if(condicao1 || condicao2) {
+    // console.log('uma delas é verdadeira')
+ //}
+//5
+ //const idadeMinima = 18
+ //const idadeDousuario = 17
+ //if (idadeMinima <= idadeDousuario) {
+    //console.log ('compra autorizada')
+ //} else {
+    //console.log('compra negada')
+ //}
+ 
---
+++ operadores-comparação.js
@@ -0,0 +1,11 @@
+const aprovado = true;
+
+if (aprovado === true) {
+    console.log ('aprovado')
+};
+
+if ('0' === 0) { 
+    console.log('true')
+} else {
+    console.log ('false')
+}
